4fbe44b11406bce53f2cc14f23350c3dd7b71ec0
Michael Salerno <msalerno1993@gmail.com>
2022-02-26 22:17:20 -0500

    added FeedbackData to store all date

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import FeedbackItem from "./components/FeedbackItem"
+import { useState } from 'react'
 import Header from "./components/Header"
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
 
 
 function App() {
+
+    const [feedback, setFeedback] = useState(FeedbackData)
+
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackItem />
+                <FeedbackList feedback={feedback}/>
             </div>
         </>  
     )
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-
-function FeedbackItem() {
-
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState('This is an example of feedback item')
-
+function FeedbackItem({item}) {
 
   return (
     <div className="card">
-        <div className="num-display" style={{backgroundColor: "#6a5acd"}}>{rating}</div>
-        <div className="text-display">{text}</div>
+        <div className="num-display" style={{backgroundColor: "#6a5acd"}}>{item.rating}</div>
+        <div className="text-display">{item.text}</div>
     </div>
   )
 
